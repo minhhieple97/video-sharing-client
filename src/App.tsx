2c028b05eea51f81f6ce37bab7a53a2d37ce0aad
hieple <hieplevuc@gmail.com>
2024-08-31 13:48:08 +0700

    init project

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <div></div>
+      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
+    </>
+  );
+}
+
+export default App;

f08635a4ec6c7fa8009b2d1f0d78057b1cae4e80
hieple <hieplevuc@gmail.com>
2024-08-31 16:11:57 +0700

    feat: Setup route

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
-import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import { Layout } from './ui/Layout';
+import PrivateRoute from './ui/PrivateRoute';
+import { VideoList } from './features/video/VideoList';
+import { LoginForm } from './features/auth/LoginForm';
+import { LoadingOverlay } from './ui/LoadingOverlay';
 
 function App() {
   return (
-    <>
-      <div></div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
-    </>
+    <Layout>
+      <LoadingOverlay />
+      <Routes>
+        <Route path="/" element={<VideoList />} />
+        <Route path="/login" element={<LoginForm />} />
+        <Route element={<PrivateRoute />}>{/* Add private routes here */}</Route>
+      </Routes>
+    </Layout>
   );
 }
 

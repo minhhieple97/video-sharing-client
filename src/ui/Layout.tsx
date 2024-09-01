a016693dca90b43aeed03c32c1a554210fc9dd1d
hieple <hieplevuc@gmail.com>
2024-09-01 08:33:30 +0700

    feat: Setup max width for layout

--- src/ui/Layout.tsx
+++ src/ui/Layout.tsx
@@ -7,7 +7,7 @@ interface LayoutProps {
 
 export const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
-    <div className="min-h-screen flex flex-col">
+    <div className="min-h-screen flex flex-col max-w-5xl mx-auto">
       <Header />
       <main className="flex-grow container mx-auto px-4 py-8">{children}</main>
     </div>

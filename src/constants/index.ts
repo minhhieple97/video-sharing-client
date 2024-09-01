55fa6879616d7e537618abd454a9872b3f3c8231
hieple <hieplevuc@gmail.com>
2024-09-01 08:27:02 +0700

    feat: Setup constants file

---
+++ src/constants/index.ts
@@ -0,0 +1,4 @@
+export const AUTH_STORAGE_KEY = 'auth_user';
+export const AUTH_EXPIRATION_KEY = 'auth_expiration';
+export const AUTH_CHECK_INTERVAL = 5 * 60 * 1000;
+export const AUTH_EXPIRATION_TIME = 60 * 60 * 1000;

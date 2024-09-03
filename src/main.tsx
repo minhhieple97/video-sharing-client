ec894d490a0ae735a9fe690226618cece2e3f2d4
hieple <hieplevuc@gmail.com>
2024-09-03 14:54:03 +0700

    chore: Remove strict mode

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,3 @@
-import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
@@ -7,13 +6,11 @@ import { AuthProvider } from './contexts/AuthContext.tsx';
 import { LoadingProvider } from './contexts/LoadingContext.tsx';
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <LoadingProvider>
-      <AuthProvider>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-      </AuthProvider>
-    </LoadingProvider>
-  </StrictMode>,
+  <LoadingProvider>
+    <AuthProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </AuthProvider>
+  </LoadingProvider>,
 );

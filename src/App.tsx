2e74d95a0355fcfa0b848cb22c87f8949a3f0ccb
hieple <hieplevuc@gmail.com>
2024-09-03 08:13:57 +0700

    feat: Add socker provider into App

--- src/App.tsx
+++ src/App.tsx
@@ -7,21 +7,24 @@ import ShareVideoForm from './features/video/ShareVideoForm';
 import { Layout } from './components/common/Layout';
 import { LoadingOverlay } from './components/ui/LoadingOverlay';
 import { GuestRoute } from './components/common/GuestRoute';
+import { SocketProvider } from './contexts/SocketContext';
 
 function App() {
   return (
-    <Layout>
-      <LoadingOverlay />
-      <Routes>
-        <Route path="/" element={<VideoList />} />
-        <Route element={<GuestRoute />}>
-          <Route path="/auth" element={<AuthForm />} />
-        </Route>
-        <Route element={<PrivateRoute />}>
-          <Route path="/share" element={<ShareVideoForm />} />
-        </Route>
-      </Routes>
-    </Layout>
+    <SocketProvider>
+      <Layout>
+        <LoadingOverlay />
+        <Routes>
+          <Route path="/" element={<VideoList />} />
+          <Route element={<GuestRoute />}>
+            <Route path="/auth" element={<AuthForm />} />
+          </Route>
+          <Route element={<PrivateRoute />}>
+            <Route path="/share" element={<ShareVideoForm />} />
+          </Route>
+        </Routes>
+      </Layout>
+    </SocketProvider>
   );
 }
 

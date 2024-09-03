dbdaaaa1db36991da3d0f2c233e995ce5fac3c14
hieple <hieplevuc@gmail.com>
2024-09-03 14:52:09 +0700

    feat: Setup call get videos endpoint

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,4 +1,4 @@
-import { AuthResponse, User } from '../interfaces';
+import { AuthResponse, User, VideoListResponse } from '../interfaces';
 import axiosInstance from '../utils/axios-config';
 export const getCurrentUser = async (): Promise<User> => {
   const response = await axiosInstance.get<User>('/auth/current-user');
@@ -17,4 +17,9 @@ export const registerUser = async (email: string, password: string): Promise<Aut
 
 export const logoutUser = async (): Promise<void> => {
   await axiosInstance.post('/auth/logout');
+};
+
+export const getVideos = async (skip: number, take: number): Promise<VideoListResponse> => {
+  const response = await axiosInstance.get<VideoListResponse>(`/videos?skip=${skip}&take=${take}`);
+  return response.data;
 };

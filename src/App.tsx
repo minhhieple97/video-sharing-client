d1e2e78fc02bb3f2852f484c4e6e2dd431fd18d8
hieple <hieplevuc@gmail.com>
2024-09-03 14:59:35 +0700

    feat: Add share video form component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import { Route, Routes } from 'react-router-dom';
 import PrivateRoute from './components/common/PrivateRoute';
-import { VideoList } from './features/video/VideoList';
+import { VideoList } from './features/video-list/VideoList';
 import { AuthForm } from './features/auth/AuthForm';
 
-import ShareVideoForm from './features/video/ShareVideoForm';
+import ShareVideoForm from './features/share-video/ShareVideoForm';
 import { Layout } from './components/common/Layout';
 import { LoadingOverlay } from './components/ui/LoadingOverlay';
 import { GuestRoute } from './components/common/GuestRoute';

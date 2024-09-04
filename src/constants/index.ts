07fc299f4b6288eaed74d1736761a4eba833e90a
hieple <hieplevuc@gmail.com>
2024-09-04 09:51:25 +0700

    feat: Update interval time chech auth

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,6 +1,6 @@
 export const AUTH_STORAGE_KEY = 'auth_user';
 export const AUTH_EXPIRATION_KEY = 'auth_expiration';
-export const AUTH_CHECK_INTERVAL = 5 * 60 * 1000;
+export const AUTH_CHECK_INTERVAL = 15 * 1000;
 export const AUTH_EXPIRATION_TIME = 60 * 60 * 1000;
 export const DEFAULT_SKIP = 0;
 export const DEFAULT_TAKE = 2;

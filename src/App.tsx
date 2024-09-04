7ad2b749dd3bb86cb985de26fcedf5976318e0b5
hieple <hieplevuc@gmail.com>
2024-09-04 09:51:09 +0700

    refactor: Move SocketProvider into main

--- src/App.tsx
+++ src/App.tsx
@@ -6,23 +6,20 @@ import { AuthForm } from './features/auth/AuthForm';
 import ShareVideoForm from './features/share-video/ShareVideoForm';
 import { Layout } from './components/common/Layout';
 import { GuestRoute } from './components/common/GuestRoute';
-import { SocketProvider } from './contexts/SocketContext';
 
 function App() {
   return (
-    <SocketProvider>
-      <Layout>
-        <Routes>
-          <Route path="/" element={<VideoList />} />
-          <Route element={<GuestRoute />}>
-            <Route path="/auth" element={<AuthForm />} />
-          </Route>
-          <Route element={<PrivateRoute />}>
-            <Route path="/share" element={<ShareVideoForm />} />
-          </Route>
-        </Routes>
-      </Layout>
-    </SocketProvider>
+    <Layout>
+      <Routes>
+        <Route path="/" element={<VideoList />} />
+        <Route element={<GuestRoute />}>
+          <Route path="/auth" element={<AuthForm />} />
+        </Route>
+        <Route element={<PrivateRoute />}>
+          <Route path="/share" element={<ShareVideoForm />} />
+        </Route>
+      </Routes>
+    </Layout>
   );
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,14 @@ import App from './App.tsx';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 import { AuthProvider } from './contexts/AuthContext.tsx';
+import { SocketProvider } from './contexts/SocketContext.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <AuthProvider>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <SocketProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </SocketProvider>
   </AuthProvider>,
 );

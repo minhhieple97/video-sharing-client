f186038b9853bc0b2815d83d9cee4242a9754adc
hieple <hieplevuc@gmail.com>
2024-09-04 11:28:53 +0700

    chore: Update default take videos

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -3,5 +3,5 @@ export const AUTH_EXPIRATION_KEY = 'auth_expiration';
 export const AUTH_CHECK_INTERVAL = 15 * 1000;
 export const AUTH_EXPIRATION_TIME = 60 * 60 * 1000;
 export const DEFAULT_SKIP = 0;
-export const DEFAULT_TAKE = 2;
+export const DEFAULT_TAKE = 10;
 export const DEFAULT_DELAY = 200;
